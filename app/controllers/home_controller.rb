9251815c91491b4c38712ac48bb6dd365c44c637
Ana <anaclimgo@gmail.com>
2025-05-24 14:51:06 -0300

    feat: added currencies for coin chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ require 'net/http'
 require 'json'
 
 class HomeController < ApplicationController
+
+  CURRENCIES = [
+    { code: 'USD-BRL'},
+    { code: 'EUR-BRL'},
+    { code: 'BTC-BRL'},
+  ]
 end
